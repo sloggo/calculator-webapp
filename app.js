a6ea269b6041a28ad83426ba69d5351522f24c50
sloggo <70816908+sloggo@users.noreply.github.com>
2022-08-16 13:50:45 +0100

    Add number button functionality

--- app.js
+++ app.js
@@ -1,28 +1,51 @@
 const operatorButtons = document.querySelectorAll('.operatorButton');
+const numberButtons = document.querySelectorAll('.numberButton')
+let currentNumber = '';
+let currentNumberDisplay = document.querySelector('.currentDisplay#number')
+
 let numberOne = null;
 let numberOneDisplay = document.querySelector('p.numberOne');
+
 let numberTwo = null;
 let numberTwoDisplay = document.querySelector('p.numberTwo');
+
 let operator = '';
 let operatorDisplay = document.querySelector('p.operatorDisplay');
 
 function operatorButtonClick(button){
     console.log(button.id+ ' clicked')
-    const currentNumber = document.querySelector('.currentDisplay#number')
     if(!numberOne){
-        numberOne = parseInt(currentNumber.textContent)
+        operator = button.id;
+        numberOne = parseInt(currentNumber)
         numberOneDisplay.textContent = numberOne
+        currentNumber='';
+        currentNumberDisplay.textContent = currentNumber;
+        operatorDisplay.textContent = button.textContent
     } else{
-        numberTwo = parseInt(currentNumber.textContent)
+        operator = button.id;
+        console.log('must operate')
+        //operate(numberOne, numberTwo, operator)
+        numberTwo = parseInt(currentNumber)
         numberTwoDisplay.textContent = numberTwo
+        currentNumber='';
+        currentNumberDisplay.textContent = currentNumber;
+        operatorDisplay.textContent = button.textContent
     }
-    operator = button.id;
-    operatorDisplay.textContent = button.textContent
-    console.log(numberOne)
-    console.log(numberTwo)
+
+}
+
+function numberButtonClick(button){
+    console.log(button.id+ ' clicked')
+    currentNumber += button.id
+    console.log('Current Number: '+currentNumber)
+    currentNumberDisplay.textContent = currentNumber
 }
 
 
 for(const button of operatorButtons){
     button.addEventListener('click', (event) => operatorButtonClick(event.target))
+}
+
+for(const button of numberButtons){
+    button.addEventListener('click', (event) => numberButtonClick(event.target))
 }
